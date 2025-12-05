f7c63cba7fb84ebd39d1f231268f1e01bb6b9b6f
Idoko Augustine <linkwithaugustine@gmail.com>
2025-12-05 11:40:16 +0100

    fixed property view to return JsonResponse

--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
@@ -8,4 +7,6 @@ def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price", "location", "created_at"
     )
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({
+        "data": list(properties)
+    })
